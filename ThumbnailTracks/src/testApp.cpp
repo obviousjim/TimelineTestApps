6c90f7d8b04672a667469998af285787aa27178a
obviousjim <james@jamesgeorge.org>
2012-07-23 20:05:48 +0900

    ThumbnailTester for image sequence and video track

---
+++ ThumbnailTracks/src/testApp.cpp
@@ -0,0 +1,77 @@
+#include "testApp.h"
+
+//--------------------------------------------------------------
+void testApp::setup(){
+	
+	ofSetFrameRate(30);
+	ofSetVerticalSync(true);
+	
+
+	timeline.setup();
+    timeline.setFrameRate(24);
+
+//	timeline.addImageSequence("testsequence");
+	
+    videoPlayer.loadMovie("/Volumes/IMPOSTER/EyeoMediaBin/Julia03/color/MVI_1007.mov"); //TODO: make common example 
+	timeline.setDurationInSeconds(videoPlayer.getPlayer()->getDuration());
+	timeline.addElement("Video", &videoPlayer);
+
+}
+
+//--------------------------------------------------------------
+void testApp::update(){
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+	ofBackground(.15*255);
+
+    
+	timeline.draw();
+ 
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::windowResized(int w, int h){
+	timeline.setWidth(w);
+}
+
+//--------------------------------------------------------------
+void testApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void testApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
